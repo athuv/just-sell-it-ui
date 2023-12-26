c445821cb0dbdf4fd6be375bd6a3dfc5f6aceaf8
Ahamed <21102277+athuv@users.noreply.github.com>
2023-12-26 22:29:07 +0530

    fix(header): fix layout shifts in header and logo components
    - Relates to: JSIUI-4, JSIUI-6

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
     <header className="h-[90px] bg-white">
       <div className="text- mx-40 flex h-full items-center justify-between px-3">
         <div className="flex items-center gap-7">
-          <div>
+          <div className="w-72">
             <LogoText fontSize={'text-4xl'} />
           </div>
           <div className="flex items-center gap-4 text-lg">
@@ -19,20 +19,22 @@ const Header = () => {
             </div>
           </div>
         </div>
-        <div className="flex items-center gap-4">
-          <button className="flex items-center gap-1">
+        <div className="flex w-fit items-center gap-4">
+          <button className="flex w-fit items-center gap-1">
             <span>
-              <BiSolidChat className="text-xl text-tertiaryBg" />
+              <BiSolidChat className="text-xl text-tertiaryBg " />
             </span>
             <span>Chat</span>
           </button>
-          <button className="flex items-center gap-1">
+          <button className="flex w-fit items-center gap-1">
             <span>
               <BiSolidUser className="text-xl text-tertiaryBg" />
             </span>
             <span>Login/Signup</span>
           </button>
-          <button className="rounded bg-tertiaryBg p-2">POST YOUR AD</button>
+          <button className="w-36 rounded bg-tertiaryBg p-2">
+            POST YOUR AD
+          </button>
         </div>
       </div>
     </header>
