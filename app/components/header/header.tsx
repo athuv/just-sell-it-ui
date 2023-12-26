241934913f5a3d7d788553f5640b64f574442d03
Ahamed <21102277+athuv@users.noreply.github.com>
2023-12-26 23:14:59 +0530

    feat(header): change navbar design for mobile first
    - Change the design of the header for mobile first and makes further
      changes to the design.
    - Relates to: JSIUI-4, JSIUI-6

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,39 +1,23 @@
 import React from 'react';
-import LogoText from '@/app/components/logo/logoText';
-import Link from 'next/link';
-import { BiSolidChat, BiSolidUser } from 'react-icons/bi';
+import { BiMenu, BiSolidChat, BiSolidUser } from 'react-icons/bi';
+import LogoText from '../logo/logoText';
 
 const Header = () => {
   return (
-    <header className="h-[90px] bg-white">
-      <div className="text- mx-40 flex h-full items-center justify-between px-3">
-        <div className="flex items-center gap-7">
-          <div className="w-72">
-            <LogoText fontSize={'text-4xl'} />
-          </div>
-          <div className="flex items-center gap-4 text-lg">
-            <Link href="/all-ads">All Ads</Link>
-            <div className="flex rounded border p-2">
-              <button className="pr-1">සිංහල</button>
-              <button className="border-l pl-1">தமிழ்</button>
-            </div>
-          </div>
+    <header className="h-16 bg-white">
+      <div className="flex h-full items-center justify-between px-4 py-1">
+        <div className="flex items-center gap-2">
+          <button className="text-4xl text-tertiaryBg">
+            <BiMenu />
+          </button>
+          <LogoText fontSize="text-2xl">J-S-I</LogoText>
         </div>
-        <div className="flex w-fit items-center gap-4">
-          <button className="flex w-fit items-center gap-1">
-            <span>
-              <BiSolidChat className="text-xl text-tertiaryBg " />
-            </span>
-            <span>Chat</span>
+        <div className="flex gap-2 text-2xl text-tertiaryBg">
+          <button>
+            <BiSolidChat />
           </button>
-          <button className="flex w-fit items-center gap-1">
-            <span>
-              <BiSolidUser className="text-xl text-tertiaryBg" />
-            </span>
-            <span>Login/Signup</span>
-          </button>
-          <button className="w-36 rounded bg-tertiaryBg p-2">
-            POST YOUR AD
+          <button>
+            <BiSolidUser />
           </button>
         </div>
       </div>
--- app/components/logo/logoText.tsx
+++ app/components/logo/logoText.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { spaceMono } from '@/app/fonts';
 
-const LogoText = ({ fontSize }: { fontSize: string }) => {
+const LogoText = ({
+  fontSize,
+  children,
+}: {
+  fontSize: string;
+  children: React.ReactNode;
+}) => {
   return (
     <span className={`${spaceMono.className} ${fontSize} text-tertiaryBg`}>
-      Just-Sell-It
+      {children}
     </span>
   );
 };
