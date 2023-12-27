f4a60ee1882ca0b3e9f38ce1c3b57fc7a26cb109
Ahamed <21102277+athuv@users.noreply.github.com>
2023-12-27 13:13:52 +0530

    feat(header): Design responsive navigation bar with sliding menu
    - implements a responsive navigation bar that displays a sliding menu on
      smaller screens (up to 768px).
    - Relates to: JSIUI-5

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { BiMenu, BiSolidChat, BiSolidUser } from 'react-icons/bi';
+import {
+  BiMenu,
+  BiSolidChat,
+  BiSolidUser,
+  BiPurchaseTagAlt,
+  BiLogIn,
+} from 'react-icons/bi';
 import LogoText from '../logo/logoText';
 
 const Header = () => {
   return (
     <header className="h-16 bg-white">
-      <div className="flex h-full items-center justify-between px-4 py-1">
+      <div className="flex h-full items-center justify-between border-b-2 border-tertiaryBg px-4 py-1">
         <div className="flex items-center gap-2">
           <button className="text-4xl text-tertiaryBg">
             <BiMenu />
@@ -21,6 +27,23 @@ const Header = () => {
           </button>
         </div>
       </div>
+      {/* Sliding Menu */}
+      <aside className="absolute h-fit w-full bg-primaryBg md:hidden">
+        <div className="flex flex-col items-start px-4 py-1">
+          <button className="flex items-center justify-center gap-2">
+            <span>
+              <BiPurchaseTagAlt />
+            </span>
+            <span>All Ads</span>
+          </button>
+          <button className="flex items-center justify-center gap-2">
+            <span>
+              <BiLogIn />
+            </span>
+            <span>Login/Register</span>
+          </button>
+        </div>
+      </aside>
     </header>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-red-700 p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between bg-green-700 p-24">
       <div>MAIN</div>
     </main>
   );
