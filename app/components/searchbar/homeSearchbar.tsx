4d703ea1d279931991940af948b83f3bc6f12e94
Ahamed <21102277+athuv@users.noreply.github.com>
2023-12-29 20:21:07 +0530

    style(search): style searchbar with relevant icons for upto 'md' screen
    - Relates to: JSIUI-6

--- app/components/searchbar/homeSearchbar.tsx
+++ app/components/searchbar/homeSearchbar.tsx
@@ -1,14 +1,45 @@
 import React from 'react';
-import { BiSearch } from 'react-icons/bi';
+import {
+  BiCategoryAlt,
+  BiLocationPlus,
+  BiSearch,
+  BiSolidKeyboard,
+} from 'react-icons/bi';
 
 const HomeSearchbar = () => {
   return (
     <section className="w-full">
-      <div className="flex flex-col gap-2 pt-3 text-quaternary">
-        <button className="h-14 w-full border">Location</button>
-        <button className="h-14 w-full border">Category</button>
-        <input className="h-14 w-full border" type="text" />
-        <button className="flex h-14 w-full flex-row items-center justify-center gap-2 border bg-tertiaryBg text-xl text-primaryBg">
+      <div
+        role="group"
+        aria-label="Search"
+        className="flex flex-col gap-2 pt-3 text-quaternary"
+      >
+        <button
+          aria-label="Location"
+          className="flex h-14 w-full items-center justify-start gap-2 border pl-4"
+        >
+          <BiLocationPlus />
+          <span>Location</span>
+        </button>
+        <button
+          aria-label="Category"
+          className="flex h-14 w-full items-center justify-start gap-2 border pl-4"
+        >
+          <BiCategoryAlt />
+          <span>Category</span>
+        </button>
+        <div className="relative">
+          <input
+            aria-label="Search"
+            placeholder="Keyword"
+            className="placeholder:pl-15 h-14 w-full border bg-primaryBg  bg-[url('/keyboard-icon.svg')] bg-[1.1rem] bg-no-repeat pl-10"
+            type="text"
+          />
+        </div>
+        <button
+          aria-label="Search"
+          className="flex h-14 w-full flex-row items-center justify-center gap-2 border bg-tertiaryBg text-xl text-primaryBg"
+        >
           <BiSearch />
           <span>Search</span>
         </button>
