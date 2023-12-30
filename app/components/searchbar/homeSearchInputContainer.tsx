fce15396ea4f0117c73cb31878e2c479590f4afc
Ahamed <21102277+athuv@users.noreply.github.com>
2023-12-30 22:35:09 +0530

    feat(search): Design location modal popup with district selection
    - add location modal with district selection popup when clicking home
      page search container location button
    - Relates to: JSIUI-6

---
+++ app/components/searchbar/homeSearchInputContainer.tsx
@@ -0,0 +1,61 @@
+import React, { Dispatch, SetStateAction } from 'react';
+import {
+  BiCategoryAlt,
+  BiLocationPlus,
+  BiSearch,
+  BiSolidKeyboard,
+} from 'react-icons/bi';
+
+const HomeSearchInputContainer = ({
+  setIsLocationOpen,
+}: {
+  setIsLocationOpen: Dispatch<SetStateAction<boolean>>;
+}) => {
+  const handleLocationClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setIsLocationOpen(true);
+  };
+
+  return (
+    <section className="w-full">
+      <div
+        role="group"
+        aria-label="Search"
+        className="flex flex-col gap-2 pt-3 text-quaternary"
+      >
+        <button
+          aria-label="Location"
+          className="flex h-14 w-full items-center justify-start gap-2 border pl-4"
+          onClick={handleLocationClick}
+        >
+          <BiLocationPlus />
+          <span>Location</span>
+        </button>
+        <button
+          aria-label="Category"
+          className="flex h-14 w-full items-center justify-start gap-2 border pl-4"
+        >
+          <BiCategoryAlt />
+          <span>Category</span>
+        </button>
+        <div className="relative">
+          <input
+            aria-label="Search"
+            placeholder="Keyword"
+            className="placeholder:pl-15 h-14 w-full border bg-primaryBg bg-[url('/keyboard-icon.svg')]  bg-[1.1rem] bg-no-repeat pl-10 placeholder:text-quaternary"
+            type="text"
+          />
+        </div>
+        <button
+          aria-label="Search"
+          className="flex h-14 w-full flex-row items-center justify-center gap-2 border bg-tertiaryBg text-xl text-primaryBg"
+        >
+          <BiSearch />
+          <span>Search</span>
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default HomeSearchInputContainer;
--- app/components/searchbar/homeSearchbar.tsx
+++ app/components/searchbar/homeSearchbar.tsx
@@ -1,50 +1,18 @@
-import React from 'react';
-import {
-  BiCategoryAlt,
-  BiLocationPlus,
-  BiSearch,
-  BiSolidKeyboard,
-} from 'react-icons/bi';
+'use client';
+import HomeSearchInputContainer from '@/app/components/searchbar/homeSearchInputContainer';
+import LocationModal from '@/app/components/searchbar/locationModal';
+import React, { useState } from 'react';
 
 const HomeSearchbar = () => {
+  const [isLocationOpen, setIsLocationOpen] = useState<boolean>(false);
+
   return (
-    <section className="w-full">
-      <div
-        role="group"
-        aria-label="Search"
-        className="flex flex-col gap-2 pt-3 text-quaternary"
-      >
-        <button
-          aria-label="Location"
-          className="flex h-14 w-full items-center justify-start gap-2 border pl-4"
-        >
-          <BiLocationPlus />
-          <span>Location</span>
-        </button>
-        <button
-          aria-label="Category"
-          className="flex h-14 w-full items-center justify-start gap-2 border pl-4"
-        >
-          <BiCategoryAlt />
-          <span>Category</span>
-        </button>
-        <div className="relative">
-          <input
-            aria-label="Search"
-            placeholder="Keyword"
-            className="placeholder:pl-15 h-14 w-full border bg-primaryBg  bg-[url('/keyboard-icon.svg')] bg-[1.1rem] bg-no-repeat pl-10"
-            type="text"
-          />
-        </div>
-        <button
-          aria-label="Search"
-          className="flex h-14 w-full flex-row items-center justify-center gap-2 border bg-tertiaryBg text-xl text-primaryBg"
-        >
-          <BiSearch />
-          <span>Search</span>
-        </button>
-      </div>
-    </section>
+    <>
+      <HomeSearchInputContainer setIsLocationOpen={setIsLocationOpen} />
+      {isLocationOpen && (
+        <LocationModal setIsLocationOpen={setIsLocationOpen} />
+      )}
+    </>
   );
 };
 
