0bdaed225e273cfcb778918ce9bf0ccd6f26c535
Ahamed <21102277+athuv@users.noreply.github.com>
2024-01-01 23:47:39 +0530

    feat(search): change ehight sizes for different screen sizes
    - Relates to: JSIUI-6

--- app/components/searchComponent/searchInput.tsx
+++ app/components/searchComponent/searchInput.tsx
@@ -3,19 +3,19 @@ import { BiSearch } from 'react-icons/bi';
 
 function SearchInput() {
   return (
-    <div className="w-full">
-      <div className="flex h-11 items-center justify-center">
+    <div className="w-full md:flex md:justify-center">
+      <div className="flex h-11 items-center justify-center md:h-14 md:w-[755px] ">
         <input
           placeholder="Keyword"
           aria-label="Search Keyword"
           type="text"
-          className="box-border h-full w-full rounded-l-3xl pl-4 focus:outline-none"
+          className="box-border h-full w-full rounded-l-3xl pl-4 focus:outline-none "
         />
-        <div className="flex h-full w-12 items-center justify-center rounded-r-3xl bg-primaryBg">
+        <div className="flex h-full w-12 items-center justify-center rounded-r-3xl bg-primaryBg md:pr-4">
           <button
             type="submit"
             aria-label="submit search"
-            className="rounded-full bg-tertiaryBg p-2 text-xl"
+            className="rounded-full bg-tertiaryBg p-2 text-xl md:text-3xl"
           >
             <BiSearch />
           </button>
