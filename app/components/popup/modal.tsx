7a179c829af0acb8a5ee55b917bdd84648389a46
Ahamed <21102277+athuv@users.noreply.github.com>
2024-01-03 00:31:00 +0530

    refactor(search): change background color and set opacity
    - Relates to: JSIUI-6

--- app/components/popup/modal.tsx
+++ app/components/popup/modal.tsx
@@ -27,9 +27,9 @@ function Modal({
   };
 
   return (
-    <div className="fixed left-0 top-0 z-20 flex h-full w-screen flex-col justify-center bg-quaternary">
-      <div className="m-4 flex h-full overflow-hidden">
-        <div className="relative block w-full overflow-y-auto rounded-xl bg-primaryBg p-4 outline-none">
+    <div className="fixed left-0 top-0 z-20 flex h-full w-screen flex-col justify-center bg-black bg-opacity-50">
+      <div className="m-4 flex h-full overflow-hidden md:justify-center">
+        <div className="relative block w-full overflow-y-auto rounded-xl bg-primaryBg p-4 outline-none md:w-[800px]">
           <div className="flex flex-col flex-nowrap content-center">
             <div className="flex justify-between">
               <div className="text-2xl">
