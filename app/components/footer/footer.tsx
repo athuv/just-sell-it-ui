6ea10aa4cdebeef1da6515a26ad368324559ae86
Ahamed <21102277+athuv@users.noreply.github.com>
2024-01-10 20:43:38 +0530

    feat(footer): Implement footer
    - Relates to: JSIUI-10

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -6,10 +6,10 @@ import Link from 'next/link';
 function Footer() {
   return (
     <footer className="border-t-2 border-tertiaryBg">
-      <div className="flex flex-col items-center justify-center px-4">
-        <div className="flex items-stretch justify-between py-4">
-          <div className="flex flex-col items-center justify-center divide-y divide-quaternary">
-            <div className="flex flex-col">
+      <div className="flex flex-col items-center justify-center px-4 lg:items-start lg:divide-y-2 xl:px-40">
+        <div className="flex items-stretch justify-between py-4 md:flex-col lg:w-full lg:flex-row lg:gap-10">
+          <div className="flex flex-col items-center justify-center divide-y divide-quaternary lg:divide-y-0">
+            <div className="flex flex-col lg:items-start">
               <h1 className="text-center font-bold">Download Our App</h1>
               <div className="flex items-center justify-center">
                 <div className="flex items-center justify-center">
@@ -30,8 +30,10 @@ function Footer() {
                 </div>
               </div>
             </div>
-            <div>
-              <h1 className="hidden text-center font-bold">Connect With</h1>
+            <div className="lg:flex lg:w-full lg:flex-col lg:items-start lg:p-3">
+              <h1 className="hidden text-center font-bold lg:block lg:text-start">
+                Connect With
+              </h1>
               <Link
                 className="flex items-center gap-1 pl-2 text-[#3b5999]"
                 href="/"
@@ -42,7 +44,7 @@ function Footer() {
                 <span>Like Us On Facebook</span>
               </Link>
             </div>
-            <div className="justify-centerdivide-x-2 flex items-center divide-x-2 p-2 text-quaternary">
+            <div className="justify-centerdivide-x-2 flex items-center divide-x-2 p-2 text-quaternary lg:hidden">
               <div className="pr-2">
                 <Link href="/">Help & Support</Link>
               </div>
@@ -50,7 +52,7 @@ function Footer() {
                 <Link href="/">More</Link>
               </div>
             </div>
-            <div className="justify-centerdivide-x-2 flex items-center divide-x-2 p-2 text-quaternary">
+            <div className="justify-centerdivide-x-2 flex items-center divide-x-2 p-2 text-quaternary lg:hidden">
               <div className="pr-2">
                 <Link href="/">සිංහල</Link>
               </div>
@@ -59,6 +61,43 @@ function Footer() {
               </div>
             </div>
           </div>
+          <div className="hidden lg:block ">
+            <h1 className="pb-4 font-bold">More From J-S-I</h1>
+            <div className="flex flex-col gap-1 text-quaternary">
+              <Link href="/">Sell Fast</Link>
+              <Link href="/">Membership</Link>
+              <Link href="/">Banner Ads</Link>
+              <Link href="/">Ad Promotions</Link>
+            </div>
+          </div>
+          <div className="hidden lg:block">
+            <h1 className="pb-4 font-bold">Help & Support</h1>
+            <div className="flex flex-col gap-1 text-quaternary">
+              <Link href="/">FAQ</Link>
+              <Link href="/">Contact Us</Link>
+            </div>
+          </div>
+          <div className="hidden lg:block">
+            <h1 className="pb-4 font-bold">Follow J-S-I</h1>
+            <div className="flex flex-col gap-1 text-quaternary">
+              <Link href="/">Blog</Link>
+              <Link href="/">Facebook</Link>
+              <Link href="/">Twitter</Link>
+              <Link href="/">Youtube</Link>
+            </div>
+          </div>
+          <div className="hidden lg:block">
+            <h1 className="pb-4 font-bold">About J-S-I</h1>
+            <div className="flex flex-col gap-1 text-quaternary">
+              <Link href="/">About Us</Link>
+              <Link href="/">Careers</Link>
+              <Link href="/">Terms & Conditions</Link>
+              <Link href="/">Privacy & Policy</Link>
+            </div>
+          </div>
+        </div>
+        <div className="p-2 text-quaternary lg:w-full">
+          Copyright &copy; Ahamed Athhar
         </div>
       </div>
     </footer>
