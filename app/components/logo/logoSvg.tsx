dc878319363d2d92e3266dc2e033f2cc5e1fbe61
Ahamed <21102277+athuv@users.noreply.github.com>
2024-01-18 19:42:38 +0530

    fix(header): fix logo default size warnings
    - Relates to: JSIUI-5

--- app/components/logo/logoSvg.tsx
+++ app/components/logo/logoSvg.tsx
@@ -6,11 +6,11 @@ import Image from 'next/image';
 function LogoSvg() {
   return (
     <>
-      <div className="md:hidden">
-        <Image src="logo-small.svg" alt="Logo" height={65} width={65} />
+      <div className="flex h-16 w-16 items-center justify-center md:hidden">
+        <Image src="logo-small.svg" alt="Logo" height={72.401} width={238.3} />
       </div>
-      <div className="hidden md:block">
-        <Image src="logo-large.svg" alt="Logo" height={65} width={200} />
+      <div className="hidden h-16 w-52 md:flex md:items-center md:justify-center">
+        <Image src="logo-large.svg" alt="Logo" height={29.121} width={286.2} />
       </div>
     </>
   );
