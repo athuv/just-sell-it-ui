968681b8537f68cfb587843cce7455adbe99d104
Ahamed <21102277+athuv@users.noreply.github.com>
2024-01-23 17:22:14 +0530

    feat(ads): Add ads page #JSIUI-18

---
+++ app/ads/page.tsx
@@ -0,0 +1,13 @@
+import MobileSearchFilter from '@/app/components/searchFilter/mobileSearchFilter';
+import React from 'react';
+
+function Allads() {
+  return (
+    <section>
+      <MobileSearchFilter />
+      <div className="px-4 py-4">All Ads</div>
+    </section>
+  );
+}
+
+export default Allads;
