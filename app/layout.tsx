c3bf38b465468bec1c33a900f123525b51340444
Ahamed <21102277+athuv@users.noreply.github.com>
2024-01-23 17:20:55 +0530

    style(body): add `h-dvh` class for body

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>
+      <body className={`${roboto.className} h-dvh`}>
         <Header />
         {children}
         {modal}
