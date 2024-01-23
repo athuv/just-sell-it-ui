9fc40fd44596a7d648e6ef5de396bf6d558ffb6e
Ahamed <21102277+athuv@users.noreply.github.com>
2024-01-23 17:32:10 +0530

    feat(ads): Add slug 'area' to show ads by slected area
    - Relates to: JSIUI-18

---
+++ app/ads/[area]/page.tsx
@@ -0,0 +1,13 @@
+import MobileSearchFilter from '@/app/components/searchFilter/mobileSearchFilter';
+import React from 'react';
+
+function AreaPage() {
+  return (
+    <section>
+      <MobileSearchFilter />
+      <div>AreaPage</div>
+    </section>
+  );
+}
+
+export default AreaPage;
