9c25596336ea8ff0b65c4111b312fb08ab8ba160
Ahamed <21102277+athuv@users.noreply.github.com>
2024-01-28 19:07:35 +0530

    feat(modal): Chnage height unit to 'vh'

--- app/components/popup/modal.tsx
+++ app/components/popup/modal.tsx
@@ -11,7 +11,7 @@ function Modal({
 }) {
   return createPortal(
     <div className="fixed left-0 top-0 z-20 flex h-full w-screen flex-col justify-center overflow-hidden overflow-x-hidden bg-black bg-opacity-50">
-      <div className="m-4 flex h-full overflow-hidden md:justify-center">
+      <div className="m-4 flex h-screen overflow-hidden md:justify-center">
         <div className="relative block w-full overflow-y-auto rounded-xl bg-primaryBg p-4 outline-none md:w-[800px]">
           <div className="text-right">
             <button
